b10629c2a508e04325dc90be7160142275c8c5b3
bharathc10 <cb987579@gmail.com>
2026-01-23 23:12:53 +0530

    testing system prompt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
@@ -22,6 +23,7 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
 
     if response.usage_metadata is None:
